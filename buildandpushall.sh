496a7cae14b2e1b0146ffe9bd0fc07053490e1f6
Jonas Doumen <jonas.doumen@kuleuven.be>
2025-06-05 14:48:33 +0200

    pushing lets taggers to docker

--- buildandpushall.sh
+++ buildandpushall.sh
@@ -9,7 +9,7 @@ docker push cclkuleuven/taggers-dockerized-base:$APP_VERSION
 docker push cclkuleuven/taggers-dockerized-spacy:$APP_VERSION
 docker push cclkuleuven/taggers-dockerized-stanza:$APP_VERSION
 # docker push cclkuleuven/taggers-dockerized-lettuce:$APP_VERSION
-# docker push cclkuleuven/taggers-dockerized-lets-nl:$APP_VERSION
-# docker push cclkuleuven/taggers-dockerized-lets-de:$APP_VERSION
-# docker push cclkuleuven/taggers-dockerized-lets-fr:$APP_VERSION
-# docker push cclkuleuven/taggers-dockerized-lets-en:$APP_VERSION
+docker push cclkuleuven/taggers-dockerized-lets-nl:$APP_VERSION
+docker push cclkuleuven/taggers-dockerized-lets-de:$APP_VERSION
+docker push cclkuleuven/taggers-dockerized-lets-fr:$APP_VERSION
+docker push cclkuleuven/taggers-dockerized-lets-en:$APP_VERSION
